ac15d7afc6087231a3234be8312227657ee72116
Haneena Ajmi <149942521+haneenaajmi@users.noreply.github.com>
2025-04-26 16:05:58 +0400

    courses

--- view/banner_Screen.dart
+++ view/banner_Screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+// import 'package:lottie/lottie.dart';
 
 class BannerScreen extends StatelessWidget {
   const BannerScreen({super.key});
@@ -6,8 +7,54 @@ class BannerScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [Text("Making education accessible to all")],
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 100, vertical: 150),
+        child: Row(
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text(
+                  "Making",
+                  style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 60,
+                      fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  "education",
+                  style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 60,
+                      fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  "accessible",
+                  style: TextStyle(
+                      color: const Color.fromARGB(255, 69, 172, 143),
+                      fontSize: 60,
+                      fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  "to all",
+                  style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 60,
+                      fontWeight: FontWeight.bold),
+                ),
+                // Center(child: Lottie.network("https://app.lottiefiles.com/share/8deafb3c-f598-4c3c-a080-0a735b6c82dd"))
+              ],
+            ),
+
+            SizedBox(width: 50),
+
+            // image
+            Container(
+              child: Image.asset("assets/images/middle_east.jpeg"),
+            )
+          ],
+        ),
       ),
     );
   }
